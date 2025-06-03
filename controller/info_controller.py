091a69b84f0a33f2b53beb4d320d44f3903ba1d8
PhucRey235 <minhphucphan235@gmail.com>
2025-06-04 03:14:28 +0700

    init

--- controller/info_controller.py
+++ controller/info_controller.py
@@ -10,6 +10,13 @@ from model.database import get_BigQuery_engine
 import pathlib
 from datetime import datetime
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)  # Cấu hình mức độ log (nếu cần)
+logger = logging.getLogger(__name__)     # Tạo logger
+
+
+
 @st.cache_resource(ttl=24*3600, max_entries=1, show_spinner=False)
 def get_thong_tin_hoc_vien(phone):
     # Lấy kết nối BigQuery
@@ -31,6 +38,7 @@ def get_thong_tin_hoc_vien(phone):
     
     result = Bigquery_db.run(query)
     print(result) 
+    logger.debug("Raw result = %s", result)
     print(1)
     # try:
     # result = ast.literal_eval(result)  # Chuyển về list chuẩn
