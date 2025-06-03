a70069c5dcef7f9778c1b0ff708655323766011a
PhucRey235 <minhphucphan235@gmail.com>
2025-06-04 03:16:05 +0700

    init

--- controller/info_controller.py
+++ controller/info_controller.py
@@ -37,10 +37,9 @@ def get_thong_tin_hoc_vien(phone):
     """
     
     result = Bigquery_db.run(query)
-    print(result) 
-    logger.debug("Raw result = %s", result)
-    print(1)
-    # try:
+    st.write(result) 
+    time.sleep(100)
+        # try:
     # result = ast.literal_eval(result)  # Chuyển về list chuẩn
     # result = dict(zip(columns, result[0]))
     # print(result)     
